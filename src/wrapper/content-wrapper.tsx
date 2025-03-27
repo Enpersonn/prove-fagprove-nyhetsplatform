56183bdfbe01ecdd622d940ae61fd0e667873bac
Enpersonn <nicklas.b.overjordet@gmail.com>
2025-03-27 16:32:21 +0100

    updated phone view support

--- src/wrapper/content-wrapper.tsx
+++ src/wrapper/content-wrapper.tsx
@@ -5,7 +5,7 @@ export default function ContentWrapper({
 }) {
   return (
     <div className="max-w-7xl w-full mx-auto grid grid-cols-12 gap-4 px-4">
-      <div className="col-start-2 col-span-10 w-full lg:col-start-3 lg:col-span-8">
+      <div className="col-span-12 md:col-start-2 md:col-span-10 w-full ">
         {children}
       </div>
     </div>
