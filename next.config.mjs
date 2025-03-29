b63730f4387065f79fe738ccf5e360d41aec58fb
Enpersonn <nicklas.b.overjordet@gmail.com>
2025-03-29 14:29:59 +0100

    tried fixing image errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-}
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "*.uploadthing.com",
+      },
+    ],
+  },
+};
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, { devBundleServerPackages: false });
